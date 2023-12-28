ad08292264403f3843f4cf3eabe67e1c4d703eb2
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-12-28 20:16:00 +0600

    header drawer design updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,46 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FaShoppingCart, FaBars } from 'react-icons/fa';
 import { FaXmark } from 'react-icons/fa6';
+import {MdHome} from 'react-icons/md';
+import {AiFillShopping} from 'react-icons/ai';
+import {RiLoginBoxLine} from 'react-icons/ri';
+import {FaArrowRightLong} from 'react-icons/fa6';
 import brandLogo from '../../assets/images/nuolar.png';
 
 export const Header = () => {
   const [drawerShow, setDrawerShow] = useState(false);
+  const drawerRef = useRef(null);
 
-  const handleDrawerClick = e => {
-    if (e.target.tagName == 'A') {
-      setDrawerShow(false);
+  useEffect(() => {
+    const handleDrawerClick = e => {
+      if (drawerRef.current && !drawerRef.current?.contains(e.target) && !(e.target.id == 'bar' || e.target.id == 'bar-container')) {
+        setDrawerShow(false);
+      }
     }
-  }
+    document.addEventListener('click', handleDrawerClick);
+
+    return () => {
+      document.removeEventListener('click', handleDrawerClick);
+    };
+  }, []); 
 
   return (
     <header className="py-4 border-b border-gray-300">
       <div className="container">
         <nav className="flex justify-between items-center gap-4">
           <div className="flex justify-center items-center gap-3">
-            <div className="md:hidden border border-gray-300 p-2 rounded text-xl cursor-pointer select-none" onClick={() => setDrawerShow(!drawerShow)}>
-              {
-                drawerShow ? <FaXmark /> : <FaBars />
-              }
+            <div className="bar md:hidden border border-gray-300 p-2 rounded text-xl cursor-pointer select-none" onClick={() => setDrawerShow(true)} id="bar-container">
+              <FaBars id="bar" />
             </div>
             <Link to='/'>
               <img src={brandLogo} alt="Brand Logo" className="w-[100px]" />
             </Link>
           </div>
 
-          <div className="flex flex-col md:flex-row justify-center items-center gap-x-6 gap-y-2 text-[18px] md:text-base font-medium fixed top-0 bottom-0 md:static bg-white md:bg-none [box-shadow:-5px_0px_50px_rgba(0,0,0,0.3)] md:shadow-none w-4/5 max-w-[300px] md:w-auto md:max-w-none transition-[right] duration-300 z-50" style={drawerShow ? {right: '0'} : {right: '-350px'}} onClick={handleDrawerClick}>
+          <div className="hidden md:flex justify-center items-center gap-6 font-medium">
             <NavLink to='/' className={({isActive}) => isActive ? 'text-primary' : ''}>Home</NavLink>
             <NavLink to='/shop' className={({isActive}) => isActive ? 'text-primary' : ''}>Shop Now</NavLink>
-            <Link to='/login' className="btn btn-secondary md:hidden mt-2">Login</Link>
           </div>
 
           <div className="flex justify-center items-center gap-4">
@@ -43,6 +52,37 @@ export const Header = () => {
             </div>
           </div>
         </nav>
+      </div>
+
+      {/* Header Drawer */}
+      <div className="md:hidden fixed top-0 bottom-0 w-3/4 max-w-[300px] bg-white z-50 [box-shadow:5px_0px_50px_rgba(0,0,0,0.3)] transition-[left] duration-300" style={drawerShow ? {left: '0'} : {left: '-350px'}} ref={drawerRef}>
+        <div className="flex justify-between items-center gap-4 px-6 py-6">
+          <h4 className="text-secondary text-xl uppercase font-semibold">Pages</h4>
+          <FaXmark className="text-2xl" onClick={() => setDrawerShow(false)} />
+        </div>
+        <div className="flex flex-col font-medium">
+          <NavLink to='/' className="flex justify-between items-center gap-3 px-6 py-3" style={({isActive}) => isActive ? {color: '#D71E44'} : {}} onClick={() => setDrawerShow(false)}>
+            <div className="flex items-center gap-3">
+              <MdHome className="text-xl" />
+              Home
+            </div>
+            <FaArrowRightLong />
+          </NavLink>
+          <NavLink to='/shop' className="flex justify-between items-center gap-3 px-6 py-3" style={({isActive}) => isActive ? {color: '#D71E44'} : {}} onClick={() => setDrawerShow(false)}>
+            <div className="flex items-center gap-3">
+              <AiFillShopping className="text-xl" />
+              Shop Now
+            </div>
+            <FaArrowRightLong />
+          </NavLink>
+          <NavLink to='/login' className="flex justify-between items-center gap-3 px-6 py-3" style={({isActive}) => isActive ? {color: '#D71E44'} : {}} onClick={() => setDrawerShow(false)}>
+            <div className="flex items-center gap-3">
+              <RiLoginBoxLine className="text-xl" />
+              Login
+            </div>
+            <FaArrowRightLong />
+          </NavLink>
+        </div>
       </div>
     </header>
   );
